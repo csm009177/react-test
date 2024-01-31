cb50d6ae303abfa5ca566a3630eeb62b908cb2b3
csm <csm009177@gmail.com>
2024-01-31 18:08:58 +0900

    install react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header"></div>
+      <div className='App-body'></div>
+      <div className='App-footer'></div>
+    </div>
+  );
+}
+
+export default App;
